4bd15dfcb8d8c57552cfcc8e24e61e271a5098be
Andrew Darqui <andrew.darqui@gmail.com>
2016-01-26 20:33:41 -0500

    formatting, so i can redo this later

--- core/nodejs/front/dbot_front.js
+++ core/nodejs/front/dbot_front.js
@@ -1,43 +1,43 @@
-//https://github.com/martynsmith/node-irc
-
-// ---- deps ----
-
 var strings;
 var d;
 var c;
 
+
+
 var initStrings = function() {
-	return {
-		config : 'config.json',
-	}
+  return {
+    config : 'config.json',
+  };
 }
+
 
 
 var initDeps = function() {
-	return {
-		irc : require('irc'),
-		net : require('net'),
-		rands : require('randstring'),
-//		config : require("./config/front/"+strings.config),
+  return {
+    irc    : require('irc'),
+    net    : require('net'),
+    rands  : require('randstring'),
     config : require(strings.config)
-	}
+  };
 }
 
-// ---- deps ----
+
 
 var initConf = function() {
-	return d.config
+  return d.config;
 }
+
 
 
 var initEnv = function() {
-	var conf = process.env['CONFIG']
-	if(conf != undefined) { strings.config = conf }
+  var conf = process.env['CONFIG']
+  if(conf != undefined) { strings.config = conf; }
 }
 
 
+
 var initId = function() {
-	c.author.id = d.rands(5)	
+  c.author.id = d.rands(5);
 }
 
 
@@ -46,49 +46,50 @@ var parseMessage = function(message) {
 /*
 {"prefix":"root!root@127.0.0.1","nick":"root","user":"root","host":"127.0.0.1","command":"PRIVMSG","rawCommand":"PRIVMSG","commandType":"normal","args":["#darqbot","^(caps (e yo))"],"server":{"name":"localhost","address":"localhost","port":6667},"author":{"name":"front","id":"ng6r6"},"type":"raw","result":"YO"}
 */
-	
-	try {
+  try {
 
-		message = JSON.parse(message)
+    message = JSON.parse(message);
 
-		var server = c.servers[message.server.name]
-		server.irc.say(message.args[0], message.result)
+    var server = c.servers[message.server.name];
+    server.irc.say(message.args[0], message.result);
 
-	} catch(err) {
-		console.log('parseMessage', err)
-	}
+  } catch(err) {
+    console.log('parseMessage', err);
+  }
+
 }
 
 
 var bindNet = function() {
-	c.net.chan = d.net.createServer(function(x) {
+  c.net.chan = d.net.createServer(function(x) {
 
-		console.log("connected")
+    console.log("connected");
 
-		try {
-			c.net.sockets[x._handle.fd] = x
-		} catch(err) {
-			console.log("err", err)
-		}
+    try {
+      c.net.sockets[x._handle.fd] = x;
+    } catch(err) {
+      console.log("bindNet: err", err);
+    }
 
-		x.on('end', function() {
-			console.log("disconnected")
-			try {
-				c.net.sockets[x._handle.fd] = {}
-			} catch(err) {
-				console.log("err", err)
-			}
-		})
-		x.on('data', function(data) {
-			data = data.toString()
-			console.log("data", data)
-			parseMessage(data)
+    x.on('end', function() {
+      console.log("disconnected");
+      try {
+        c.net.sockets[x._handle.fd] = {};
+      } catch(err) {
+        console.log("err", err);
+      }
+    });
 
-		})
-	})	
-	c.net.chan.listen(c.net.port, c.net.host, function() {
-		console.log("bind")
-	})
+    x.on('data', function(data) {
+      data = data.toString();
+      console.log("data", data);
+      parseMessage(data);
+    });
+  });
+
+  c.net.chan.listen(c.net.port, c.net.host, function() {
+    console.log("bind");
+  });
 }
 
 
@@ -97,29 +98,28 @@ var bindNet = function() {
 
 var sendAll = function(type,message) {
 
-		message.author = c.author
-		message.type = type
+  message.author = c.author;
+  message.type = type;
 
-        for(var v in c.net.sockets) {
-            var socket = c.net.sockets[v]
-			try {
-            	socket.write(JSON.stringify(message))
-			} catch(err) {
-				c.net.sockets[v] = {}
-			}
-        }
-
+  for(var v in c.net.sockets) {
+    var socket = c.net.sockets[v];
+    try {
+      socket.write(JSON.stringify(message));
+    } catch(err) {
+      c.net.sockets[v] = {};
+    }
+  }
 }
 
 
 
 var appendMessage = function(message,server) {
-	message.server = {
-		author : server.author,
-		name : server.name,
-		address : server.address,
-		port : server.port,
-	}
+  message.server = {
+    author : server.author,
+    name : server.name,
+    address : server.address,
+    port : server.port,
+  };
 }
 
 
@@ -127,49 +127,51 @@ var appendMessage = function(message,server) {
 
 var connectIrc = function() {
 
-	for(var v in c.servers) {
-		var server = c.servers[v]
+  for(var v in c.servers) {
+    var server = c.servers[v];
 
-		server.irc = {}
+    server.irc = {};
 
-		server.irc = new d.irc.Client(
-			server.address,
-			server.nick,
-			server
-		)
+    server.irc = new d.irc.Client(
+      server.address,
+      server.nick,
+      server
+    );
 
-		server.name = v
+    server.name = v;
 
-		server.irc.addListener('raw', (function(__server) {
-			return function(message) {
-				appendMessage(message,__server)
-				sendAll('raw',message)
-			}
-		})(server))
+    server.irc.addListener('raw', (function(__server) {
+      return function(message) {
+        appendMessage(message,__server);
+        sendAll('raw',message);
+      }
+    })(server));
 
-		server.irc.addListener('error', (function(__server) {
-			return function(message) {
-				console.log("error", message)
-				appendMessage(message,__server)
-				sendAll('error',message)
-			}
-		})(server))
-	}
+    server.irc.addListener('error', (function(__server) {
+      return function(message) {
+        console.log("error", message);
+        appendMessage(message,__server);
+        sendAll('error',message);
+      }
+    })(server));
+  }
 }
 
 
 
 var init = function() {
 
-	strings = initStrings()
-	initEnv()
-	d = initDeps()
-	c = initConf()
-	initId()
+  strings = initStrings();
+  initEnv();
+  d = initDeps();
+  c = initConf();
+  c.net.sockets = [];
+  initId();
 
-	connectIrc()
-	bindNet()
+  connectIrc();
+  bindNet();
+
 }
 
 
-init()
+init();
