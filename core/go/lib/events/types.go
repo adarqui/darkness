a06eb17b95d34b7cf7ab528c948a76646c948555
Andrew Darqui <andrew.darqui@gmail.com>
2016-01-28 11:37:44 -0500

    event types

---
+++ core/go/lib/events/types.go
@@ -0,0 +1,15 @@
+package darkness_events
+
+const (
+  EVENT_NOP = iota
+  EVENT_DIE = iota
+  EVENT_FRONT_CONNECTED = iota
+  EVENT_TUNNEL_CONNECTED = iota
+  EVENT_FRONT_RECEIVED_MESSAGE = iota
+  EVENT_TUNNEL_SEND_MESSAGE = iota
+)
+
+type Event struct {
+  Type int `json:"type"`
+  Payload []byte `json:"payload"`
+}
