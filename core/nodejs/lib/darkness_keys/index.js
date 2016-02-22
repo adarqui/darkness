181dafc037afaf81028dc88ec4079f9617054f7e
Andrew Darqui <andrew.darqui@gmail.com>
2016-02-21 19:12:34 -0500

    keys

--- core/nodejs/lib/darkness_keys/index.js
+++ core/nodejs/lib/darkness_keys/index.js
@@ -1,8 +1,11 @@
-
+(function() {
+"use strict";
 
 module.exports = {
-  DARK_COUNTER: "dark:counter",
-  DARK_EVENT: "dark:event",
-  DARK_RELAY: "dark:relay",
+  DARK_COUNTER:      "dark:counter",
+  DARK_EVENT:        "dark:event",
+  DARK_RELAY:        "dark:relay",
   DARK_INTERP_REPLY: "dark:interp:reply"
-}
+};
+
+})();
