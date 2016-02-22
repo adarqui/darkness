4744e8e66b97400da87ea0ace0f8613b39eb9db8
Andrew Darqui <andrew.darqui@gmail.com>
2016-02-21 21:51:52 -0500

    oops

--- core/nodejs/lib/darkness_keys/index.js
+++ core/nodejs/lib/darkness_keys/index.js
@@ -1,11 +1,53 @@
 (function() {
 "use strict";
 
-module.exports = {
-  DARK_COUNTER:      "dark:counter",
-  DARK_EVENT:        "dark:event",
-  DARK_RELAY:        "dark:relay",
-  DARK_INTERP_REPLY: "dark:interp:reply"
+
+
+var exports = module.exports = {};
+
+
+
+exports.DARK_COUNTER      = "dark:counter";
+exports.DARK_EVENT        = "dark:event";
+exports.DARK_RELAY        = "dark:relay";
+exports.DARK_INTERP_REPLY = "dark:interp:reply";
+
+
+
+/*
+ * DARK_COUNTER:<label>
+ */
+exports.mkCounter = function(key) {
+  return DARK_COUNTER + ":" + key;
 };
+
+
+
+/*
+ * DARK_EVENT
+ */
+exports.mkEvent = function() {
+  return DARK_EVENT;
+};
+
+
+
+/*
+ * DARK_RELAY
+ */
+exports.mkRelay = function() {
+  return DARK_RELAY;
+};
+
+
+
+/*
+* DARK_RELAY:<SERVER>
+ */
+exports.mkRelayServer = function(key) {
+  return DARK_RELAY + ":" + key;
+};
+
+
 
 })();
