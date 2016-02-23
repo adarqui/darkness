eb091e6173c516fe3daf3a47805b177aee3f3ba8
Andrew Darqui <andrew.darqui@gmail.com>
2016-02-23 18:48:12 -0500

    order matters

--- launch/start_simple.sh
+++ launch/start_simple.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# start relay daemon
-(cd core/go/relay; make build; make run &)
+# order matters..
 
 # start irc connected daemon
 (cd core/go/proto/irc/connected; make build; make run &)
+
+# start relay daemon
+(cd core/go/relay; make build; make run &)
--- launch/stop_simple.sh
+++ launch/stop_simple.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# stop relay daemon
-(cd core/go/relay; make kill)
-
 # stop irc connected daemon
 (cd core/go/proto/irc/connected; make kill)
+
+# stop relay daemon
+(cd core/go/relay; make kill)
