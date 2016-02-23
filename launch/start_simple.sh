10f3541a293f6489a6a9e005856c5d88acca16f0
Andrew Darqui <andrew.darqui@gmail.com>
2016-02-23 18:46:10 -0500

    irc connected daemon support

--- launch/start_simple.sh
+++ launch/start_simple.sh
@@ -2,3 +2,6 @@
 
 # start relay daemon
 (cd core/go/relay; make build; make run &)
+
+# start irc connected daemon
+(cd core/go/proto/irc/connected; make build; make run &)
--- launch/stop_simple.sh
+++ launch/stop_simple.sh
@@ -2,3 +2,6 @@
 
 # stop relay daemon
 (cd core/go/relay; make kill)
+
+# stop irc connected daemon
+(cd core/go/proto/irc/connected; make kill)
