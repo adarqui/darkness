63c0862c007588f9d543e5291288ca5e2ca036ca
Andrew Darqui <andrew.darqui@gmail.com>
2016-02-23 13:04:59 -0500

    dno

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,11 @@ mkdir -p ~/build/github.com/adarqui
 
 export GOPATH=~/build/
 
-(cd ~/build/github.com/adarqui; git clone https://github.com/adarqui/darkness)
+cd ~/build/github.com/adarqui
+
+if [ ! -d darkness ] ; then
+  git clone https://github.com/adarqui/darkness
+else
+  cd darkness
+  git pull origin master
+fi
