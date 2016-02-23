47f93d43d82de8250fcfe7471d390cd31b134c00
Andrew Darqui <andrew.darqui@gmail.com>
2016-02-23 18:53:54 -0500

    ping daemon

--- launch/start_simple.sh
+++ launch/start_simple.sh
@@ -5,5 +5,8 @@
 # start irc connected daemon
 (cd core/go/proto/irc/connected; make build; make run &)
 
+# start irc ping daemon
+(cd core/go/proto/irc/ping; make build; make run &)
+
 # start relay daemon
 (cd core/go/relay; make build; make run &)
--- launch/stop_simple.sh
+++ launch/stop_simple.sh
@@ -3,5 +3,8 @@
 # stop irc connected daemon
 (cd core/go/proto/irc/connected; make kill)
 
+# stop irc ping daemon
+(cd core/go/proto/irc/ping; make kill)
+
 # stop relay daemon
 (cd core/go/relay; make kill)
