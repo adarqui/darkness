8cfd8fef90a929b2501d48935dcd5e121d5fbe0a
Andrew Darqui <andrew.darqui@gmail.com>
2016-02-28 10:39:14 -0500

    tmux script to open windows for every language in commands/

---
+++ bin/tmux-commands.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SESSION="dark-commands"
+IGNORE_DIRS="private"
+
+tmux new-session -d -s ${SESSION}
+
+COUNT=1
+find . -type d -d 1 | egrep -vi "${IGNORE_DIRS}" | while read dir ; do
+  dir_stripped=`basename "${dir}"`
+  tmux new-window -t $SESSION:"${COUNT}" -n "${dir_stripped}"
+  tmux split-window
+  tmux select-pane -t 0
+  tmux send-keys "cd ${dir_stripped}" C-m
+  tmux select-pane -t 1
+  tmux send-keys "cd ${dir_stripped}" C-m
+  COUNT=$((COUNT+1))
+done
