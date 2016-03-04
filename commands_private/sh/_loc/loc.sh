a092276646ef2f0d60165d269ecb8d358e55935a
Andrew Darqui <andrew.darqui@gmail.com>
2016-03-04 01:03:12 -0500

    private commands, not executable by the executor.. simple lines of code script

---
+++ commands_private/sh/_loc/loc.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+function join {
+  local d=$1;
+  shift;
+  printf "$1";
+  shift;
+  printf "%s" "${@/#/$d}";
+}
+
+
+
+if [ $# -lt 2 ] ; then
+  echo "usage: $0 <dir> <file_extension_1> <file_extensions_2>";
+  echo "ex: $0 ./ .hs .c .purs";
+  exit 1;
+fi
+
+args=( ${@} );
+
+declare -a arr;
+index=0;
+for i in ${args[@]:1}; do
+  arr[index]="*$i";
+  index=$((index+1));
+done
+
+extensions="`join ' -or -name ' ${arr[@]}`";
+
+find "$1" -type f \( -name $extensions \) -exec wc -l {} \; | awk '{print $1}' | paste -s -d+ -| bc;
