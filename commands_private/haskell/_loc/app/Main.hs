30d368830a280d2f796242d607acbffed26ccbe5
Andrew Darqui <andrew.darqui@gmail.com>
2016-03-08 03:09:39 -0500

    loc in haskell using filemanip

---
+++ commands_private/haskell/_loc/app/Main.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import           Darkness.Commands.Private.LOC (countLinesOfFiles,
+                                                findFilesBySuffix)
+import           System.Environment            (getArgs)
+
+
+
+usage :: IO ()
+usage = do
+  putStrLn $
+    unlines
+      [
+        "usage: loc <directory> <file_extension_1> ... <file_extension_n"
+      , "ex: loc ./ .hs .c .purs"
+      ]
+
+
+
+main :: IO ()
+main = do
+
+  argv <- getArgs
+  case argv of
+    (directory:file_extensions) -> do
+      results <- findFilesBySuffix file_extensions directory >>= countLinesOfFiles
+      putStrLn $ show results
+      return ()
+    _ -> usage
---
+++ commands_private/haskell/_loc/src/Darkness/Commands/Private/LOC.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Darkness.Commands.Private.LOC (
+  findFilesBySuffix,
+  countLinesOfFiles
+) where
+
+import           System.FilePath.Find (FileType (..), always, extension,
+                                       fileType, find, (&&?), (==?))
+
+import           System.Directory     (canonicalizePath)
+
+import qualified Data.Text            as T (lines)
+import qualified Data.Text.IO         as T (readFile)
+
+
+
+findFilesBySuffix :: [String] -> FilePath -> IO [FilePath]
+findFilesBySuffix suffixes path = do
+  canonical <- canonicalizePath path
+  find
+    always
+    (
+      fileType ==? RegularFile &&?
+      (extension >>= \ext -> return $ elem ext suffixes)
+    )
+    canonical
+
+
+-- | TODO: Need to handle failure to open due to perms etc.
+--
+countLinesOfFiles :: [FilePath] -> IO Int
+countLinesOfFiles files =
+  sum <$> mapM (\file -> (length . T.lines) <$> T.readFile file) files
