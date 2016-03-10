e934d15baf2c1697b6cc37501144072ade27e0f0
Andrew Darqui <andrew.darqui@gmail.com>
2016-03-10 01:02:07 -0500

    adds -n argument but.. still prints NL at the end.. wtf?

--- commands/ada/_echo/echo.adb
+++ commands/ada/_echo/echo.adb
@@ -2,9 +2,17 @@ with Ada.Command_Line; use Ada.Command_Line;
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Echo is
+  opt_n : Boolean := True;
+  start_index : Integer := 1;
 begin
 
-  for index in 1..Argument_Count loop
+  if Argument_Count > 0 and then Argument(1) = "-n"
+  then
+    opt_n := False;
+    start_index := 2;
+  end if;
+
+  for index in start_index..Argument_Count loop
     Put(Argument(index));
     if index /= Argument_Count
     then
@@ -12,6 +20,9 @@ begin
     end if;
   end loop;
 
-  New_Line;
+  if opt_n = True
+  then
+    New_Line;
+  end if;
 
 end;
