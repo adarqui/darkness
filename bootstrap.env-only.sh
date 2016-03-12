ab8752f3eb8960c11502a6f0046b65b195c17276
Andrew Darqui <andrew.darqui@gmail.com>
2016-03-12 00:48:20 -0500

    new env vars

--- bootstrap.env-only.sh
+++ bootstrap.env-only.sh
@@ -12,7 +12,10 @@ fi
 # env variables
 #
 export DARK_ROOT="${HOME}/build/src/github.com/adarqui/darkness"
+export DARK_DATA="${DARK_ROOT}/data"
 export DARK_LOGS="${DARK_ROOT}/logs"
 export DARK_CMD="${DARK_ROOT}/commands"
+export DARK_CMD_PRIVATE="${DARK_ROOT}/commands_private"
+
 export GOPATH="${HOME}/build/"
 export NODE_PATH="${DARK_ROOT}/core/nodejs/lib/"
