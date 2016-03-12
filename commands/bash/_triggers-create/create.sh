08e172cdc29c61fd10028bdef0c472a46457120a
Andrew Darqui <andrew.darqui@gmail.com>
2016-03-12 17:11:35 -0500

    temporary: create a trigger

---
+++ commands/bash/_triggers-create/create.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+export PATH="$PATH:${DARK_ROOT}/listeners/haskell/triggers/bin"
+
+if (( $# <= 1 )) ; then
+  echo "usage: $0 <key> value for key.." && exit 1
+fi
+
+key="${1}"
+shift
+value="$@"
+
+user_info="${DARK_EXEC_IRC_USER}"
+label="${DARK_EXEC_SERVER_LABEL}"
+channel="${DARK_EXEC_IRC_CHANNEL#\#}"
+nick="${DARK_EXEC_IRC_NICK}"
+ns="${label}:${channel}"
+
+value=`triggers-cli create-trigger "${ns}" "${key}" "${value}" "${nick}" "${user_info}" 2>/dev/null`
+
+if (( $? != 0 )) ; then
+  echo "unable to add key."
+  exit 1
+fi
+
+echo "${key} successfully added."
