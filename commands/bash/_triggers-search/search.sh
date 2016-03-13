67bc62215026f02059ea8f39841e510f98e78f8c
Andrew Darqui <andrew.darqui@gmail.com>
2016-03-13 04:08:36 -0400

    triggers search tool

---
+++ commands/bash/_triggers-search/search.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+export PATH="$PATH:${DARK_ROOT}/listeners/haskell/triggers/bin"
+
+if (( $# < 3 )) ; then
+  echo "usage: $0 <offset:0-x> <search_by:{key,value,author}> <keywords>" && exit 1
+fi
+
+offset="$1"
+search_by=`echo "$2" | tr '[:upper:]' '[:lower:]'`
+shift 2
+keywords="$@"
+
+if [[ "${search_by}" != "key" && "${search_by}" != "value" && "${search_by}" != "author" ]]; then
+  echo 'search_by must be either: key, value, or author' && exit 1
+fi
+
+label="${DARK_EXEC_SERVER_LABEL}"
+channel="${DARK_EXEC_IRC_CHANNEL#\#}"
+nick="${DARK_EXEC_IRC_NICK}"
+ns="${label}:${channel}"
+
+result=`triggers-cli search-triggers "${ns}" "${keywords}" "${search_by}" 20 "${offset}" 2>/dev/null`
+
+if (( $? != 0 )) || [ "${result}" == "[]" ]; then
+  echo "search criteria not found."
+  exit 1
+fi
+
+keys=`echo "${result}" | jq -r '[.[] | .key] | @csv'`
+keys_=${keys/\",\"/ , }
+keys__=${keys_//\"}
+echo "${keys__}"
