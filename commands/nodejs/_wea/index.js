7d62df2c42cb244c3b4f23d6d457b2d158c5f5db
Andrew Darqui <andrew.darqui@gmail.com>
2016-03-15 02:00:37 -0400

    might as well use promises instead of try catch

--- commands/nodejs/_wea/index.js
+++ commands/nodejs/_wea/index.js
@@ -10,8 +10,9 @@
   };
 
   var
+    Bluebird = require("bluebird"),
     Weather = require("weather-zip"),
-    FS = require("fs")
+    FS = Bluebird.promisifyAll(require("fs"))
     ;
 
   if (process.env.DARK_CMD === undefined) {
@@ -21,8 +22,6 @@
 
   var
     argv,
-    forecastio_key,
-    weather,
     zip
     ;
 
@@ -33,38 +32,44 @@
 
   zip = argv[2];
 
-  try {
-    forecastio_key = FS.readFileSync(process.env.DARK_CMD + "/etc/keys/forecast.io.txt").toString().trim();
-  } catch(err) {
-    console.error("API key not found.");
-    process.exit(1);
-  }
-
-  weather = new Weather(forecastio_key);
-
-  weather.get(zip)
-  .then(function (data, err){
-    // ugly.. we might need different printers, for example: irc, cli
-    // this goes for all darkness commands..
-    // this is condensed, abbreviated one line output is suitable for irc
-    console.log(
-      "(" +
-        "currently: " + data.currently.summary + ", " +
-        "precip-probability: " + (data.currently.precipProbability * 100) + "%, " +
-        "temperature: " + data.currently.temperature + "F, " +
-        "humidity: " + (data.currently.humidity * 100) + "%, " +
-        "wind-speed: " + data.currently.windSpeed + "mph) " +
-      "(minutely: " + data.minutely.summary + ") " +
-      "(hourly: " + data.hourly.summary + ") " +
-      "(daily: " + data.daily.summary + ")"
-    );
+  var promise = FS.readFileAsync(process.env.DARK_CMD + "/etc/keys/forecast.io.txt").then(function(data) {
+    return data.toString().trim();
   })
 
   .error(function(err) {
-    if (err) {
-      console.error("unable to find weather data for " + zip);
-      process.exit(1);
-    }
+    console.error("API key not found.");
+    process.exit(1);
+  });
+
+  promise.then(function(forecastio_key) {
+
+    var weather = new Weather(forecastio_key);
+
+    weather.get(zip)
+    .then(function (data){
+      // ugly.. we might need different printers, for example: irc, cli
+      // this goes for all darkness commands..
+      // this is condensed, abbreviated one line output is suitable for irc
+      console.log(
+        "(" +
+          "currently: " + data.currently.summary + ", " +
+          "precip-probability: " + (data.currently.precipProbability * 100) + "%, " +
+          "temperature: " + data.currently.temperature + "F, " +
+          "humidity: " + (data.currently.humidity * 100) + "%, " +
+          "wind-speed: " + data.currently.windSpeed + "mph) " +
+        "(minutely: " + data.minutely.summary + ") " +
+        "(hourly: " + data.hourly.summary + ") " +
+        "(daily: " + data.daily.summary + ")"
+      );
+    })
+
+    .error(function(err) {
+      if (err) {
+        console.error("unable to find weather data for " + zip);
+        process.exit(1);
+      }
+    });
+
   });
 
 
