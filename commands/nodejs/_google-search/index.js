f31b8626a84616b54a3af56c5599956b56860f7f
Andrew Darqui <andrew.darqui@gmail.com>
2016-03-15 21:01:11 -0400

    error handling..

--- commands/nodejs/_google-search/index.js
+++ commands/nodejs/_google-search/index.js
@@ -5,7 +5,7 @@
 
   // maybe add optional results argument (argv[3])
   if (process.argv.length != 3) {
-    console.log("usage: google-search <search string>");
+    console.error("usage: google-search <search string>");
     process.exit(1);
   }
 
@@ -22,18 +22,14 @@
       process.exit(1);
     }
 
+    if (links === undefined) {
+      console.error("No results.");
+      process.exit(1);
+    }
+
     for (var i = 0; i < (links.length > max_results ? max_results : links.length); ++i) {
       console.log(links[i].title + ' - ' + links[i].link);
     }
-
-    /*
-    if (nextCounter < 4) {
-      nextCounter += 1;
-      if (next) {
-        next();
-      }
-    }
-    */
 
   });
 }) ();
